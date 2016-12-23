d88628a06014be613d5084d11d5cb40fbcdd75d5
dimitar-pechev <dimpechev@gmail.com>
2016-12-23 03:25:46 +0200

    removed forgotten redundant dependency

--- server/server.js
+++ server/server.js
@@ -2,24 +2,10 @@
 
 const config = require('./config'),
     app = require('./config/express'),
-    data = require('./data'),
-    http = require('http');
+    data = require('./data');
 
 require('./config/mongoose')(config);
 require('./config/passport')(app, data);
 require('./routers')(app, data);
 
-//app.listen(config.port, () => console.log('Server now up and running at http://localhost:3000'));
-
-const port = process.env.PORT || '3000';
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+app.listen(config.port, () => console.log(`Server now up and running at http://localhost:${config.port}`));
