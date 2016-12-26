58110c2462aef57db6bd2f4f0cef470fac620416
dimitar-pechev <dimpechev@gmail.com>
2016-12-27 01:51:55 +0200

    reg form init. just the layout for now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { FooterComponent } from './navigation/footer/footer.component';
 import { HomeComponent } from './home/home.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainNavigationComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
