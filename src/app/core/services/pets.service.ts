10a1a54aa599e44e8d5ec09891caf2a05cac3821
MyamotoMusashi <knifed313@gmail.com>
2016-12-28 18:39:12 +0200

    About,Videos,Pictures,Family Tree now show and hide on click

--- src/app/core/services/pets.service.ts
+++ src/app/core/services/pets.service.ts
@@ -8,6 +8,7 @@ export class PetService {
         years: 3,
         weight: 39,
         imgSrc: "http://cdn1-www.dogtime.com/assets/uploads/gallery/labrador-retriever-dog-breed-pictures/labrador-retriever-dog-pictures-1.jpg",
+        about: "Весел и енергичен, млад Лабрадор",
         posts: [{ caption: "Във Враца", info: { where: 'Парк "Мир и Дружба", Перник' }, imgSrc: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTmZQ6f6vXaFvi1lsKZzFK-9s8oxtJnduuNfjKLWSe8Yo7DjF1L", description: "Какъв прекрасен ден за разходка. Докато се разхождах из този чуден парк във Перник, спрях и се замислих, какво ли щях да правя сега във Враца ?" },
         { caption: "Със племенника", info: { where: 'Във: Парк "Нова Надежда", Врацa', with: "Snoopy" }, imgSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9MO9gAVKGZwYVEK09PVI0__nYcn3qGmHk7CRyk47LJPFheySdOg", description: "#chill-вам с племенника." }]
     },
--- src/app/pets/pet-profile-component/pet-profile.component.ts
+++ src/app/pets/pet-profile-component/pet-profile.component.ts
@@ -14,6 +14,11 @@ export class PetProfileComponent implements OnInit {
   pets: any[] = [];
   pet: any;
   url: any[];
+  isAboutShowed: boolean = true;
+  isFamilyTreeShowed: boolean = true;
+  isPicturesShowed: boolean = true;
+  isVideosShowed: boolean = true;
+  
 
   constructor(private petService: PetService, private route: ActivatedRoute) { }
 
@@ -21,5 +26,61 @@ export class PetProfileComponent implements OnInit {
     this.pets = this.petService.getAll();
     this.pet = this.petService.getById(this.route.snapshot.params['id']);
     this.url = this.route.snapshot.url;
+  }
+
+  showAbout(){
+    if (this.isAboutShowed == false){
+      this.isAboutShowed = true;
+    }
+    else{
+      this.isAboutShowed = false;
+    }
+    this.isFamilyTreeShowed = false;
+    this.isVideosShowed = false;
+    this.isPicturesShowed = false;
+    
+    console.log(this.isAboutShowed);
+  }
+
+  showFamilyTree(){
+    if (this.isFamilyTreeShowed == false){
+      this.isFamilyTreeShowed = true;
+    }
+    else{
+      this.isFamilyTreeShowed = false;
+    }
+    this.isAboutShowed = false;
+    this.isVideosShowed = false;
+    this.isPicturesShowed = false;
+    
+    console.log(this.isAboutShowed);
+  }
+
+  showPictures(){
+    if (this.isPicturesShowed == false){
+      this.isPicturesShowed = true;
+    }
+    else{
+      this.isPicturesShowed = false;
+    }
+    this.isAboutShowed = false;
+    this.isFamilyTreeShowed = false;
+    this.isVideosShowed = false;
+    
+    console.log(this.isAboutShowed);
+  }
+
+    showVideos(){
+    if (this.isVideosShowed == false){
+      this.isVideosShowed = true;
+    }
+    else{
+      this.isVideosShowed = false;
+    }
+    this.isAboutShowed = false;
+    this.isFamilyTreeShowed = false;
+    this.isPicturesShowed = false;
+    
+    console.log(this.isAboutShowed);
   }
 }
